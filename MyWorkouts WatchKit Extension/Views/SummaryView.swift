024e6b32209f078ce5e70facbb3ef006ed9a915e
Uday Pandey <uday.pandey@gmail.com>
2021-06-15 06:53:19 +0100

    Finished linking all current screens together

---
+++ MyWorkouts WatchKit Extension/Views/SummaryView.swift
@@ -0,0 +1,83 @@
+//
+//  SummaryView.swift
+//  MyWorkouts WatchKit Extension
+//
+//  Created by Uday Pandey on 15/06/2021.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    @State private var durationFormatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.zeroFormattingBehavior = .pad
+        return formatter
+    }()
+
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack(alignment: .leading) {
+                SummaryMetricView(
+                    title: "Total Time",
+                    value: durationFormatter.string(from: 30 * 60 + 15) ?? "")
+                    .accentColor(.yellow)
+
+                SummaryMetricView(
+                    title: "Total Distance",
+                    value:
+                        Measurement(value: 1625, unit: UnitLength.meters)
+                        .formatted(.measurement(width: .abbreviated, usage: .road))
+                )
+                .accentColor(.green)
+
+                SummaryMetricView(
+                    title: "Total Energy",
+                    value:
+                        Measurement(value: 96, unit: UnitEnergy.kilocalories)
+                        .formatted(.measurement(width: .abbreviated, usage: .workout, numberFormat: .numeric(precision: .fractionLength(0))))
+                )
+                .accentColor(.pink)
+
+                SummaryMetricView(
+                    title: "Avg. Heart Rate",
+                    value: 143
+                        .formatted(.number.precision(.fractionLength(0)))
+                    + " bpm"
+                    )
+                .accentColor(.red)
+
+                Button("Done") {
+
+                }
+            }
+            .scenePadding()
+        }
+        .navigationTitle("Summary")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct SummaryMetricView: View {
+    var title: String
+    var value: String
+
+    var body: some View {
+        Text(title)
+        Text(value)
+            .font(
+                .system(.title2, design: .rounded)
+                    .lowercaseSmallCaps()
+            )
+            .foregroundColor(.accentColor)
+
+        Divider()
+
+
+    }
+}
+struct SummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryView()
+    }
+}
